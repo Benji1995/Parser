2fc0760cea956a944a4022c135953fc1642f9b38
Benji1995 <benjamin.bernaerdts@gmail.com>
2022-04-28 21:30:32 +0200

    buh

--- Parsing_Classes/InvoiceParser.py
+++ Parsing_Classes/InvoiceParser.py
@@ -1,7 +1,6 @@
 import dataclasses
 from typing import List
-
-from SQL_Classes.DB_Agent import DB_Agent
+# from ...SQLPackage.DB_Agent import DB_Agent
 from .PDFParser import PDFParser
 from static.DataClasses.dc_transaction import Transaction
 
@@ -24,24 +23,24 @@ class InvoiceParser(PDFParser):
     def label_transaction(self):
         pass
 
-    def send_trans_to_DB(self):
-        db_agent = DB_Agent()
+    # def send_trans_to_DB(self):
+    #     db_agent = DB_Agent()
         
-        if not db_agent.db_connection_healthy:
-            print("With creating a Database Agent, the connection could not be guaranteed so shutting down..")
-            exit()
+    #     if not db_agent.db_connection_healthy:
+    #         print("With creating a Database Agent, the connection could not be guaranteed so shutting down..")
+    #         exit()
 
-        for transaction in self.list_of_transactions:
-            self.send_tran_to_DB(db_agent, transaction)
-        pass
+    #     for transaction in self.list_of_transactions:
+    #         self.send_tran_to_DB(db_agent, transaction)
+    #     pass
 
-    def send_tran_to_DB(self, db_agent, transaction):
-        trans_str=list(dataclasses.asdict(transaction).values())
-        trans_str[0]=trans_str[0].isoformat()
-        trans_str=[str(x) for x in trans_str]
-        trans_str[0]="".join(["\'",trans_str[0],"\'"])
-        trans_str[-2]="".join(["\'",trans_str[-2][0:125].replace("\'",""),"\'"])
-        trans_str[-1]="".join(["\'",trans_str[-1],"\'"])
+    # def send_tran_to_DB(self, db_agent, transaction):
+    #     trans_str=list(dataclasses.asdict(transaction).values())
+    #     trans_str[0]=trans_str[0].isoformat()
+    #     trans_str=[str(x) for x in trans_str]
+    #     trans_str[0]="".join(["\'",trans_str[0],"\'"])
+    #     trans_str[-2]="".join(["\'",trans_str[-2][0:125].replace("\'",""),"\'"])
+    #     trans_str[-1]="".join(["\'",trans_str[-1],"\'"])
 
-        db_agent.insert_record("tblTransactions",trans_str)
-        pass
+    #     db_agent.insert_record("tblTransactions",trans_str)
+    #     pass
--- main.py
+++ main.py
@@ -1,9 +1,14 @@
 from Parsing_Classes.BelfiusInvoiceParser import BelfiusInvoiceParser
-from ..SQLPackage import DB_Init
+from SQLPackage import DB_Init
 from ..GoogleDriveAPI import GSheets
 from ..TKInterface import Interface
 import os
 
+interface = Interface.Interface()
+options = {1:'Single File Operation', 2:'Entire Folder Operation'}
+option = interface.askOptions(options)
+print(option)
+exit()
 folder = "C:/Users/pek3nm/Downloads/Uitreksels"
 
 for file in os.listdir(folder):
