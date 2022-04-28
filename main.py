ed7dee844a95d1aad37a6ac5206f7db96ef4f55a
Benji1995 <benjamin.bernaerdts@gmail.com>
2022-04-28 20:49:22 +0200

    cleaning up

--- main.py
+++ main.py
@@ -1,12 +1,14 @@
 from Parsing_Classes.BelfiusInvoiceParser import BelfiusInvoiceParser
-from SQL_Classes.DB_Init import DB_Init
+from ..SQLPackage import DB_Init
 from ..GoogleDriveAPI import GSheets
+from ..TKInterface import Interface
 import os
 
-# folder = "C:/Users/pek3nm/Downloads/Uitreksels"
-# for file in os.listdir(folder):
-#     bel_pars = BelfiusInvoiceParser("/".join([folder,file]))
-#     bel_pars.getInvoice()
+folder = "C:/Users/pek3nm/Downloads/Uitreksels"
+
+for file in os.listdir(folder):
+    bel_pars = BelfiusInvoiceParser("/".join([folder,file]))
+    bel_pars.getInvoice()
 
 # mysql_agent = DB_Init()
 
