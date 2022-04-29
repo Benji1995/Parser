7fcf738138e3e4195bdc22743dac4c419568d363
Benji1995 <benjamin.bernaerdts@gmail.com>
2022-04-29 17:59:57 +0200

    voor het weekend

--- main.py
+++ main.py
@@ -1,20 +1,31 @@
 from Parsing_Classes.BelfiusInvoiceParser import BelfiusInvoiceParser
-from ..SQLPackage import DB_Init
+import ..SQLPackage
 from ..GoogleDriveAPI import GSheets
 from ..TKInterface import Interface
 import os
 
+"Prepare interface to Proceed with process.."
 interface = Interface.Interface()
-options = {1:'Single File Operation', 2:'Entire Folder Operation'}
-option = interface.askOptions(options)
-print(option)
-exit()
-folder = "C:/Users/pek3nm/Downloads/Uitreksels"
 
-for file in os.listdir(folder):
+"Options to be displayed to user"
+options = {1:'Single File Operation', 2:'Entire Folder Operation'}
+
+"Decision made by the user, depending on the options."
+option = interface.askOptions(options)
+
+"Execute appropriate action based on the option"
+if option == 1:
+
+    "Single File Process"
     bel_pars = BelfiusInvoiceParser("/".join([folder,file]))
+    
+    "Retrieve Invoices"
     bel_pars.getInvoice()
 
-# mysql_agent = DB_Init()
-
-
+elif option == 2:
+    "Entire Folder Process"
+    folder = interface.askFolder()
+    
+    for file in os.listdir(folder):
+        bel_pars = BelfiusInvoiceParser("/".join([folder,file]))
+        bel_pars.getInvoice()
