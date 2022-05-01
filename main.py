718f833353af35e29bb84855ce9a74f4a119ab9a
Benji1995 <benjamin.bernaerdts@gmail.com>
2022-05-01 15:44:04 +0100

    Resolved Import error for windows

--- main.py
+++ main.py
@@ -1,7 +1,18 @@
 from Parsing_Classes.BelfiusInvoiceParser import BelfiusInvoiceParser
-import ..SQLPackage
-from ..GoogleDriveAPI import GSheets
-from ..TKInterface import Interface
+import sys
+
+try:
+    from ..GoogleDriveAPI import GSheets
+except ImportError:
+    sys.path.append("..\GoogleDriveAPI")
+    import GSheets
+
+try:
+    from ..TKInterface import Interface
+except ImportError:
+    sys.path.append("..\TKInterface")
+    import Interface
+
 import os
 
 "Prepare interface to Proceed with process.."
@@ -13,19 +24,27 @@ options = {1:'Single File Operation', 2:'Entire Folder Operation'}
 "Decision made by the user, depending on the options."
 option = interface.askOptions(options)
 
-"Execute appropriate action based on the option"
-if option == 1:
-
-    "Single File Process"
-    bel_pars = BelfiusInvoiceParser("/".join([folder,file]))
-    
-    "Retrieve Invoices"
-    bel_pars.getInvoice()
-
-elif option == 2:
-    "Entire Folder Process"
-    folder = interface.askFolder()
-    
-    for file in os.listdir(folder):
-        bel_pars = BelfiusInvoiceParser("/".join([folder,file]))
+try:
+        
+    "Execute appropriate action based on the option"
+    if option == 1:
+        "Ask for File"
+        file = interface.askFile()
+        
+        "Single File Process"
+        bel_pars = BelfiusInvoiceParser(file)
+        
+        "Retrieve Invoices"
         bel_pars.getInvoice()
+
+    elif option == 2:
+        "Entire Folder Process"
+        folder = interface.askFolder()
+        
+        for file in os.listdir(folder):
+            bel_pars = BelfiusInvoiceParser("/".join([folder,file]))
+            bel_pars.getInvoice()
+
+    print(bel_pars.list_of_transactions)
+except FileNotFoundError:
+    raise FileNotFoundError('Action has been cancelled..')
